fd68556cf3422f821be748b26fae01880c1b55bf
Swatee <swatee.salunkhe@eres.ae>
2018-06-27 10:42:43 +0400

    Initial Commit

---
+++ EresScanner/EresScanner.h
@@ -0,0 +1,19 @@
+//
+//  EresScanner.h
+//  EresScanner
+//
+//  Created by Swatee Salunkhe on 6/27/18.
+//  Copyright © 2018 Swatee Salunkhe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EresScanner.
+FOUNDATION_EXPORT double EresScannerVersionNumber;
+
+//! Project version string for EresScanner.
+FOUNDATION_EXPORT const unsigned char EresScannerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EresScanner/PublicHeader.h>
+
+
